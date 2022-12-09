cbfb33f28216dde7be49321862cd2118061f692d
Maiky1304 <maiky-perlee@outlook.com>
2022-12-09 23:59:48 +0100

    setup prisma + migration

---
+++ prisma/migrations/20221209225914_store/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "stores" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "stores_pkey" PRIMARY KEY ("id")
+);
